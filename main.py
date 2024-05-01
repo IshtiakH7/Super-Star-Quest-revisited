e1c553f724f7c281e387a1dafff73bc07c649d63
i_chowdhury18 <i_chowdhury18@cranford.hounslow.sch.uk>
2024-05-01 11:29:56 +0100

    made main start

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+#initialise pygame
+pygame.init()
+
+#sets the game display name at window top
+pygame.display.set_caption("Super Star Quest")
+
+#set window size,colour,fps and speed of user movement
+BG_COLOUR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#EVENT LOOP
+def main(window):
+    clock = pygame.time.clock
+#event loop while loop, ensures FPS
+    run = True
+    while run:
+        clock.tick(FPS)
+
+
+#ONLY CALLING THIS FUNCTION IF CALLED, NOT RUN WHEN BEING CALLED FROM OTHER FILES/FUNCTIONS
+if __name__=="__main__":
+    main(window)

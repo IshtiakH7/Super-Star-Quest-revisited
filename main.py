5282d9e624f446bba7b3e5aebaf57ed8cea4e5bc
i_chowdhury18 <i_chowdhury18@cranford.hounslow.sch.uk>
2024-05-01 12:44:29 +0100

    made background

--- main.py
+++ main.py
@@ -11,17 +11,37 @@ pygame.init()
 #sets the game display name at window top
 pygame.display.set_caption("Super Star Quest")
 
-#set window size,colour,fps and speed of user movement
-BG_COLOUR = (255, 255, 255)
+#set window size,fps and speed of user movement
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join("Background", "Green.png"))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+
 #EVENT LOOP
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Green.png")
 #event loop while loop, ensures FPS
     run = True
     while run:
@@ -31,6 +51,8 @@ def main(window):
             if event.type ==pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
 
